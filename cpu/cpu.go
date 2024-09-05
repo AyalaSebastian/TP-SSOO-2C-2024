6cf811576dceb9ce5b5aa55f911ffe2f16aa35d6
Lucas Cristin <lukicristin@gmail.com>
2024-09-04 21:47:39 -0300

    Se desarrollaron los log de los modulos FS y CPU

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"log/slog"
+
+	"github.com/sisoputnfrba/tp-golang/utils/logging"
+)
+
 func main() {
 
 	// Inicio configs
 	config = Iniciar_configuracion("config.json")
+
+	// Inicio logger
+	Logger := logging.IniciarLogger("cpu.log", config.LogLevel)
+
+	// Compruebo que funcione el logger
+	Logger.Info("Logger iniciado")
+	slog.Info("Logger iniciado")
 
 }
--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log/slog"
+
+	"github.com/sisoputnfrba/tp-golang/utils/logging"
+)
+
 func main() {
 
+	// Inicio configs
+	config = Iniciar_configuracion("config.json")
+
+	// Inicio log
+	log := logging.IniciarLogger("filesystem.log", config.LogLevel)
+
+	log.Info("Logger iniciado")
+	slog.Info("Logger iniciado")
 }
