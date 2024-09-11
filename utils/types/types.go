90f337d94fce500f0d675fdd8029b1b76cb42434
Alexander <litovchenko5108@gmail.com>
2024-09-11 10:25:28 -0300

    agregué estructura PCB y TCB

--- utils/types/types.go
+++ utils/types/types.go
@@ -3,3 +3,17 @@ package types
 type HandShake struct {
 	Mensaje string `json:"mensaje"`
 }
+
+type PCB struct {
+	PID    uint32   `json:"pid"`
+	TCBs   []TCB    `json:"tcb"`
+	Mutexs []string `json:"mutexs"`
+}
+
+type TCB struct {
+	TID       uint32 `json:"tid"`
+	Prioridad string `json:"prioridad"`
+	Estado    string `json:"estado"` //Puede ser "NEW", "READY", "EXECUTE", "BLOCKED", "EXIT" (En mayusculas)
+	PID       uint32 `json:"pid"`    //PID del proceso al que pertenece
+
+}
