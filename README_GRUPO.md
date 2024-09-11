514102cfeceb5847bab5517c68fe8ac513afb752
Lucas Cristin <lukicristin@gmail.com>
2024-09-11 14:59:52 -0300

    Se agreagaron las estructuras regCPU y Contecto de ejecucion para el futuro manejo de los modulos. Tambien se agrego un readme para la comunicacion del grupo

---
+++ README_GRUPO.md
@@ -0,0 +1,8 @@
+# formato para q sea mas legible el codigo 
+1. Variables en camelCase
+2. Funciones con separadas con _ 
+
+
+# Preguntas para los soportes
+1. Cuando llega una systemcall se hacen dos conexiones (memoria,kernel) o solo memoria y ella le avisa a kernel? 
+
--- utils/types/types.go
+++ utils/types/types.go
@@ -7,13 +7,31 @@ type HandShake struct {
 type PCB struct {
 	PID    uint32   `json:"pid"`
 	TCBs   []TCB    `json:"tcb"`
-	Mutexs []string `json:"mutexs"`
+	Mutexs []string `json:"mutexs"` // los podemos representar con 0 y 1 (como el bitarray)
 }
 
 type TCB struct {
 	TID       uint32 `json:"tid"`
 	Prioridad string `json:"prioridad"`
-	Estado    string `json:"estado"` //Puede ser "NEW", "READY", "EXECUTE", "BLOCKED", "EXIT" (En mayusculas)
-	PID       uint32 `json:"pid"`    //PID del proceso al que pertenece
+	Estado    string `json:"estado"`    //Puede ser "NEW", "READY", "EXECUTE", "BLOCKED", "EXIT" (En mayusculas)
+	PID       uint32 `json:"pid"`       //PID del proceso al que pertenece
+	Registros RegCPU `json:"registros"` //! (Verificar si esta bien)
+}
 
+type RegCPU struct {
+	PC uint32 `json:"pc"` // Program Counter (Proxima instruccion a ejecutar)
+	AX uint32 `json:"ax"`
+	BX uint32 `json:"bx"`
+	CX uint32 `json:"cx"`
+	DX uint32 `json:"dx"`
+	EX uint32 `json:"ex"`
+	FX uint32 `json:"fx"`
+	GX uint32 `json:"gx"`
+	HX uint32 `json:"hx"`
+}
+
+type ContextoEjecucion struct {
+	Base      int    `json:"base"`
+	Limite    int    `json:"limite"`
+	Registros RegCPU `json:"registros"`
 }
