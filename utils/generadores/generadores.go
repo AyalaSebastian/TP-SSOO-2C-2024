646d60ca34bc1a2eec92bec0ada0d472f894c742
Alexander <litovchenko5108@gmail.com>
2024-09-22 20:29:57 -0300

    creé archivo de generadores (pcb, tcb)

---
+++ utils/generadores/generadores.go
@@ -0,0 +1,33 @@
+package generadores
+
+import (
+	"github.com/sisoputnfrba/tp-golang/utils/types"
+)
+
+var PidCounter uint32 = 0
+
+// Genera un PID único (el tipo de dato uint32 es para que no tome valore negativos).
+func Generar_PID() uint32 {
+	PidCounter++
+	return PidCounter
+}
+
+// Genera un PCB con un PID único y con las listas de TCBs y Mutexs vacías.
+func Generar_PCB() types.PCB {
+	return types.PCB{
+		PID:    Generar_PID(),
+		TCBs:   []types.TCB{},
+		Mutexs: []string{},
+	}
+}
+
+// Genera un nuevo TCB y lo añade al PCB recibido por parámetro (pasar el pcb con &).
+func Generar_TCB(pcb *types.PCB, prioridad int) types.TCB {
+	tid := len(pcb.TCBs) // Usamos la longitud actual de TCBs para generar el próximo TID.
+	tcb := types.TCB{
+		TID:       tid,
+		Prioridad: prioridad,
+	}
+	pcb.TCBs = append(pcb.TCBs, tcb) // Añadimos el nuevo TCB al PCB.
+	return tcb
+}
