4cb6dfe2394097c0c8390fd828301da4581a5f1f
Alexander <litovchenko5108@gmail.com>
2024-09-25 13:50:13 -0300

    iniciación de colas y primer proceso

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 	"os"
 	"strconv"
 
-	"github.com/sisoputnfrba/tp-golang/kernel/client"
+	"github.com/sisoputnfrba/tp-golang/kernel/planificador"
 	"github.com/sisoputnfrba/tp-golang/kernel/server"
 	"github.com/sisoputnfrba/tp-golang/kernel/utils"
-	"github.com/sisoputnfrba/tp-golang/utils/generadores"
 	"github.com/sisoputnfrba/tp-golang/utils/logging"
-	"github.com/sisoputnfrba/tp-golang/utils/types"
 )
-
-// Inicializamos las colas de procesos
-var colaNew []types.PCB   //Cola de procesos nuevos (Manejada por FIFO)
-var colaReady []types.TCB // Aca tengo dudas de como es, no me queda claro si las colas son distintas para PCB y TCB
 
 func main() {
 
 	// Inicializamos la configuracion y el logger
 	utils.Configs = utils.Iniciar_Configuracion("config.json")
 	logger := logging.Iniciar_Logger("kernel.log", utils.Configs.LogLevel)
+
+	// Inicializamos las colas de procesos
+	planificador.Inicializar_colas()
 
 	// Obtener los parametros del primer proceso a ejecutar
 	archivoPseudocodigo := os.Args[1]
@@ -33,48 +29,8 @@ func main() {
 	}
 
 	// Creación del proceso inicial
-	crear_proceso(archivoPseudocodigo, tamanioProceso, 0, logger)
+	planificador.Crear_proceso(archivoPseudocodigo, tamanioProceso, 0, logger)
 
 	// Iniciamos Kernel como server
-
 	server.Iniciar_kernel(logger)
-}
-
-// Acá para mi hay que mandar el path a memoria para que saque las instrucciones del archivo de pseudocódigo y acá mismo armar el PCB con el TCB y todo
-func crear_proceso(pseudo string, tamanio int, prioridad int, logger *slog.Logger) {
-	pcb := generadores.Generar_PCB()
-	logger.Info(fmt.Sprintf("## (%d:0) Se crea el proceso - Estado: NEW", pcb.PID))
-	if colaNew == nil {
-		// Enviar a memoria el archivo de pseudocódigo y el tamaño del proceso
-		success := client.Enviar_parametros_proceso(utils.Configs.IpMemory, utils.Configs.PortMemory, pseudo, tamanio, logger)
-
-		if success {
-			tcb := generadores.Generar_TCB(&pcb, prioridad)
-			utils.Encolar(&colaReady, tcb)
-			logger.Info(fmt.Sprintf("## (%d:%d) Se crea el Hilo - Estado: READY", pcb.PID, tcb.TID))
-		} else {
-			logger.Error("No se pudo asignar espacio en memoria para el proceso")
-			utils.Encolar(&colaNew, pcb)
-		}
-	}
-}
-
-// tcb := generadores.Generar_TCB(&pcb, 0)
-
-//todo Lo que tiene que hacer la funcion
-//Para la creación de hilos, el Kernel deberá informar a la Memoria y luego
-//ingresarlo directamente a la cola de READY correspondiente, según su nivel de prioridad.
-
-func crear_hilo() {
-
-}
-
-//todo Lo que tiene que hacer la funcion
-//Al momento de finalizar un hilo, el Kernel deberá informar a la Memoria la finalización del mismo y
-//deberá mover al estado READY a todos los hilos que se encontraban bloqueados por ese TID. De esta
-//manera, se desbloquean aquellos hilos bloqueados por THREAD_JOIN y por mutex tomados por el
-//hilo finalizado (en caso que hubiera).
-
-func finalizar_hilo() {
-
 }
