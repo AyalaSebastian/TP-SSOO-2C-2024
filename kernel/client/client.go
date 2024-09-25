54771f55c310c178515a64fff9843f54d25f785b
Alexander <litovchenko5108@gmail.com>
2024-09-25 13:51:24 -0300

    funciones para enviar datos con Body y Query Path

--- kernel/client/client.go
+++ kernel/client/client.go
@@ -6,42 +6,10 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-
-	"github.com/sisoputnfrba/tp-golang/utils/types"
 )
 
-// Devuelve true si memoria pudo crear el proceso, false en caso contrario
-func Enviar_parametros_proceso(ip string, puerto int, path string, tamanio int, logger *slog.Logger) bool {
-	mensaje := types.PathTamanio{Path: path, Tamanio: tamanio}
-	body, err := json.Marshal(mensaje)
-	if err != nil {
-		logger.Error("Se produjo un error codificando el mensaje")
-		return false
-	}
-	// Siento que esto lo podriamos modularizar en una funcion que reciba el ip, puerto, body y el endpoint
-	url := fmt.Sprintf("http://%s:%d/crear-proceso", ip, puerto)
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
-	if err != nil {
-		logger.Info("Se produjo un error enviando mensaje a ip:%s puerto:%d", ip, puerto)
-		return false
-	}
-	// Aseguramos que el body sea cerrado
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		logger.Info("La respuesta del servidor no fue OK")
-		return false // Indica que la respuesta no fue exitosa
-	}
-
-	logger.Info(fmt.Sprintf("Respuesta del servidor: %s", resp.Status))
-	return true // Indica que la respuesta fue exitosa
-}
-
-func Informar_memoria_creacion_hilo() {
-
-}
-
-func Enviar_server[T any](dato T, ip string, puerto int, endpoint string, logger *slog.Logger) bool {
+// Devuelve true en caso de que la respuesta del servidor sea exitosa, false en caso contrario
+func Enviar_Body[T any](dato T, ip string, puerto int, endpoint string, logger *slog.Logger) bool {
 
 	body, err := json.Marshal(dato)
 	if err != nil {
@@ -52,17 +20,49 @@ func Enviar_server[T any](dato T, ip string, puerto int, endpoint string, logger
 	url := fmt.Sprintf("http://%s:%d/%s", ip, puerto, endpoint)
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
 	if err != nil {
-		logger.Info("Se produjo un error enviando mensaje a ip:%s puerto:%d", ip, puerto)
+		logger.Error(fmt.Sprintf("Se produjo un error enviando mensaje a ip:%s puerto:%d", ip, puerto))
 		return false
 	}
 	// Aseguramos que el body sea cerrado
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		logger.Info("La respuesta del servidor no fue OK")
+		logger.Error("La respuesta del servidor no fue OK")
 		return false // Indica que la respuesta no fue exitosa
 	}
 
 	logger.Info(fmt.Sprintf("Respuesta del servidor: %s", resp.Status))
 	return true // Indica que la respuesta fue exitosa
+}
+
+// IMPORTANTE! Es QueryPath, no se le pasa un Body. Tener en cuenta que tiene verbo PATCH, de ultima si necesitamos otro verbo podemos hacerla mas generica
+func Enviar_QueryPath[T any](dato T, ip string, puerto int, endpoint string, logger *slog.Logger) bool {
+	cliente := &http.Client{}
+	url := fmt.Sprintf("http://%s:%d/%s/%v", ip, puerto, endpoint, dato)
+	req, err := http.NewRequest("PATCH", url, nil)
+	if err != nil {
+		return false
+	}
+	// Establecer el encabezado Content-Type
+	req.Header.Set("Content-Type", "application/json")
+
+	// Enviar la solicitud
+	resp, err := cliente.Do(req)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error al enviar la solicitud: %v", err))
+		return false
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Error("La respuesta del servidor no fue OK")
+		return false // Indica que la respuesta no fue exitosa
+	}
+
+	logger.Info(fmt.Sprintf("Respuesta del servidor: %s", resp.Status))
+	return true // Indica que la respuesta fue exitosa
+}
+
+func Informar_memoria_creacion_hilo() {
+
 }
