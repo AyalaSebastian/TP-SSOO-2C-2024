cf336247fe1c2e770c07abb830adabf3e471c667
Alexander <litovchenko5108@gmail.com>
2024-10-19 18:38:11 -0300

    borré lineas que ya no servían

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	// "strconv"
-	// "sync"
-
 	"github.com/sisoputnfrba/tp-golang/filesystem/utils"
 
-	// "github.com/sisoputnfrba/tp-golang/utils/client"
 	"github.com/sisoputnfrba/tp-golang/utils/logging"
 )
-
-// var waitGroup sync.WaitGroup
 
 func main() {
 
@@ -26,16 +20,5 @@ func main() {
 
 	// Iniciar filesystem como server
 	utils.Iniciar_fileSystem(logger)
-	// waitGroup.Add(1)
 
-	// Handshakes a memoria
-	// 	ipMemoryParceado, err := strconv.Atoi(utils.Configs.IpMemory)
-	// 	if err != nil {
-	// 		logger.Error(err.Error())
-
-	// 	}
-
-	// 	client.Enviar_handshake(strconv.Itoa(utils.Configs.PortMemory), ipMemoryParceado, "Estableciendo handshake con Memoria desde Filesystem")
-
-	// 	// waitGroup.Wait()
 }
