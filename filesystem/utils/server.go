c1d2edff81ab51a36601e38558b42972de47495d
Alexander <litovchenko5108@gmail.com>
2024-11-07 01:19:10 -0300

    comienzo manejo de dump

--- filesystem/utils/server.go
+++ filesystem/utils/server.go
@@ -1,19 +1,38 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"strconv"
 
 	"github.com/sisoputnfrba/tp-golang/utils/conexiones"
-	"github.com/sisoputnfrba/tp-golang/utils/server"
+	"github.com/sisoputnfrba/tp-golang/utils/types"
 )
 
 func Iniciar_fileSystem(logger *slog.Logger) {
 	mux := http.NewServeMux()
 
 	// Endpoints
-	mux.HandleFunc("/handshake", server.Recibir_handshake(logger))
+	mux.HandleFunc("/dump", DUMP(logger))
 
 	conexiones.LevantarServidor(strconv.Itoa(Configs.Port), mux, logger)
+}
+
+func DUMP(logger *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var magic types.DumpFile
+		err := decoder.Decode(&magic)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error al decodificar mensaje: %s\n", err.Error()))
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Error al decodificar mensaje"))
+			return
+		}
+		// sigue el codigo
+
+		// Verificar si se cuenta con el espacio disponible
+	}
 }
